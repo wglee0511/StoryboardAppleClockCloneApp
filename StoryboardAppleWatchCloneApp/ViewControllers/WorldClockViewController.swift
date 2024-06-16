e2092e14d95b264a52dfd1668ee4b37dd31afd95
wglee0511 <ranazang@naver.com>
2024-06-17 02:55:57 +0900

    feature: add first tab

---
+++ StoryboardAppleWatchCloneApp/ViewControllers/WorldClockViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockViewController.swift
+//  StoryboardAppleWatchCloneApp
+//
+//  Created by racoon on 6/17/24.
+//
+
+import UIKit
+
+class WorldClockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

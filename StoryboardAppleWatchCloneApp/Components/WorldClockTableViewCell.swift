daf7057700149c0008b3fac818094f3cc036d696
wglee0511 <ranazang@naver.com>
2024-06-17 03:31:43 +0900

    feature: table view cell ui

---
+++ StoryboardAppleWatchCloneApp/Components/WorldClockTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  WorldClockTableViewCell.swift
+//  StoryboardAppleWatchCloneApp
+//
+//  Created by racoon on 6/17/24.
+//
+
+import UIKit
+
+class WorldClockTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tiimeOffsetLabel: UILabel!
+    @IBOutlet weak var timezoneLabel: UILabel!
+    @IBOutlet weak var timePeriodLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- StoryboardAppleWatchCloneApp/ViewControllers/WorldClockViewController.swift
+++ StoryboardAppleWatchCloneApp/ViewControllers/WorldClockViewController.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class WorldClockViewController: UIViewController {
 
+    @IBOutlet weak var worldClockTableView: UITableView!
+    
+    var timeZoneList = [
+        TimeZone(identifier: "Asia/Seoul")!,
+        TimeZone(identifier: "Europe/Paris")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
+}
+
+extension WorldClockViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return timeZoneList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: WorldClockTableViewCell.self), for: indexPath)
+        
+        return cell
+    }
+}
+
+extension WorldClockViewController: UITableViewDelegate {
+    
 }

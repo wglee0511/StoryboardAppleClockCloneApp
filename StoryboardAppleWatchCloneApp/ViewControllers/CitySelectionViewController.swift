0a01b695d6af5af9dafa55fe299ac63c37f82ff6
wglee0511 <ranazang@naver.com>
2024-06-18 00:01:11 +0900

    feature: search cities

--- StoryboardAppleWatchCloneApp/ViewControllers/CitySelectionViewController.swift
+++ StoryboardAppleWatchCloneApp/ViewControllers/CitySelectionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class CitySelectionViewController: UIViewController {
     
     var cities: [Section] = []
+    var tempCities: [Section] = []
 
     @IBOutlet weak var cityTableView: UITableView!
     
@@ -53,10 +54,33 @@ class CitySelectionViewController: UIViewController {
         }
         
         cities.sort { $0.sectionTitle < $1.sectionTitle }
+        tempCities = cities
     }
     
+    @objc func onPressCancelButton() {
+        dismiss(animated: true)
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "검색"
+        searchBar.delegate = self
+        
+        let cancelButton = UIButton(type: .custom)
+        cancelButton.setTitle("취소", for: .normal)
+        cancelButton.setTitleColor(.systemOrange, for: .normal)
+        cancelButton.addTarget(self, action: #selector(onPressCancelButton), for: .touchUpInside)
+        cancelButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 40).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [searchBar, cancelButton])
+        stackView.spacing = 8
+        stackView.axis = .horizontal
+        stackView.widthAnchor.constraint(greaterThanOrEqualToConstant: view.bounds.width - 32).isActive = true
+        navigationItem.titleView = stackView
+        
+        
         setUpCities()
         cityTableView.reloadData()
         // Do any additional setup after loading the view.
@@ -65,13 +89,38 @@ class CitySelectionViewController: UIViewController {
 
 }
 
+extension CitySelectionViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else {
+            tempCities = cities
+            cityTableView.reloadData()
+            cityTableView.isHidden = false
+            return
+        }
+        
+        tempCities.removeAll()
+        
+        for city in self.cities {
+            let filteredItems = city.items.filter { $0.title.lowercased().contains(searchText.lowercased()) }
+            
+            
+            if !filteredItems.isEmpty {
+                tempCities.append(Section(sectionTitle: city.sectionTitle, items: filteredItems))
+            }
+        }
+        
+        cityTableView.reloadData()
+        cityTableView.isHidden = tempCities.isEmpty
+    }
+}
+
 extension CitySelectionViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return cities.count
+        return tempCities.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cities[section].items.count
+        return tempCities[section].items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -80,13 +129,13 @@ extension CitySelectionViewController: UITableViewDataSource {
         let section = indexPath.section
         let row = indexPath.row
         
-        cell.cityTextLabel.text = cities[section].items[row].title
+        cell.cityTextLabel.text = tempCities[section].items[row].title
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return cities[section].sectionTitle
+        return tempCities[section].sectionTitle
     }
     
     func sectionIndexTitles(for tableView: UITableView) -> [String]? {
@@ -96,6 +145,6 @@ extension CitySelectionViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
         
-        return cities.firstIndex { $0.sectionTitle.uppercased() == title.uppercased() } ?? 0
+        return tempCities.firstIndex { $0.sectionTitle.uppercased() == title.uppercased() } ?? 0
     }
 }
